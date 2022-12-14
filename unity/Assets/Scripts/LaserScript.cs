8b249d8c4c2237f408a853f86a930d1d0ae5a717
Lukas Feldgrill <64472839+lufeld@users.noreply.github.com>
2022-12-14 14:38:42 +0100

    fixed PR errors; weapon balancing

--- unity/Assets/Scripts/LaserScript.cs
+++ unity/Assets/Scripts/LaserScript.cs
@@ -10,23 +10,17 @@ public class LaserScript : MonoBehaviour
 
     private LineRenderer lineRenderer;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //this.gameObject.SetActive(false);
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.useWorldSpace = true;
         lineRenderer.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 mousePos = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        //float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-
-        //transform.rotation = Quaternion.Euler(0, 0, angle - 90);
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxRange, layersToHit);
         Debug.DrawLine(transform.position, hit.point);
@@ -46,13 +40,11 @@ public class LaserScript : MonoBehaviour
 
     public void Shoot()
     {
-        //this.gameObject.SetActive(true);
         lineRenderer.enabled = true;
     }
 
     public void StopShooting()
     {
-        //this.gameObject.SetActive(false);
         lineRenderer.enabled = false;
     }
 }
