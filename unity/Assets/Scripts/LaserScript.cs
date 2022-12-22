8b90fa4373a81f4a05d239b235c8d99b754fea24
Lukas Feldgrill <64472839+lufeld@users.noreply.github.com>
2022-12-22 18:42:53 +0100

    fixed laser

--- unity/Assets/Scripts/LaserScript.cs
+++ unity/Assets/Scripts/LaserScript.cs
@@ -6,15 +6,19 @@ public class LaserScript : MonoBehaviour
 {
     public float maxRange;
     public LayerMask layersToHit;
-    public float damage;
+    public int damage;
 
     private LineRenderer lineRenderer;
+    private float damageDelay = 1f / 2f;
+    private bool doDamage = false;
 
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.useWorldSpace = true;
         lineRenderer.enabled = false;
+
+        InvokeRepeating("EnableDamage", 0, damageDelay);
     }
 
     void Update()
@@ -27,13 +31,14 @@ public class LaserScript : MonoBehaviour
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, hit.point);
 
-        if (hit.collider != null)
+        if (hit.collider != null && lineRenderer.enabled)
         {
             //Debug.Log(hit.collider.gameObject.name);
 
-            if (hit.collider.tag == "Enemy")
+            if (hit.collider.tag == "Enemy" && doDamage)
             {
-                // damage enemy
+                hit.collider.gameObject.GetComponent<EnemyBehavior>().TakeDamage(damage, new Vector2(0,0));
+                doDamage = false;
             }
         }
     }
@@ -46,5 +51,10 @@ public class LaserScript : MonoBehaviour
     public void StopShooting()
     {
         lineRenderer.enabled = false;
+    }
+
+    private void EnableDamage()
+    {
+        doDamage = true;
     }
 }
